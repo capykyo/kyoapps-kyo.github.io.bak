c9d690c715c699d24e3edf21f4474eea5d1e4f75
kyoapps-kyo <kyoapps-kyo@users.noreply.github.com>
2022-11-21 19:23:40 +0000

    deploy: 7a0d62699c56f740d3795ed4dff6e2537bcca478

--- _nuxt/static/1669058027/articles/article-1/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1669058027",layout:"default",error:a,serverRendered:true,routePath:"\u002Farticles\u002Farticle-1",config:{baseUrl:"https:\u002F\u002Fubdmf.github.io\u002F",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"06fc5ca3"}}}}(null));
--- _nuxt/static/1669058027/tags/说明文档/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1669058027",layout:"default",error:a,serverRendered:true,routePath:"\u002Ftags\u002F%E8%AF%B4%E6%98%8E%E6%96%87%E6%A1%A3",config:{baseUrl:"https:\u002F\u002Fubdmf.github.io\u002F",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"06fc5ca3"}}}}(null));
