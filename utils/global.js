1020099286384b7cc1e6e5d1e84f350ba399014e
kyoapps-kyo <kyo@kyoapps.com>
2022-11-22 04:12:46 +0900

    update siteUrl

--- utils/global.js
+++ utils/global.js
@@ -4,7 +4,7 @@
   access any variable via object syntax global.yourVarName
 */
 export default {
-  siteUrl: 'https://ubdmf.github.io',
+  siteUrl: 'https://kyoapps-kyo.github.io/',
   siteName: 'Little Star',
   author: 'WXX',
   twitterHandle: '',
