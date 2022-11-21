5057273fc3c8701818051786be01945f94966d55
kyoapps-kyo <kyoapps-kyo@users.noreply.github.com>
2022-11-21 19:13:47 +0000

    deploy: 1020099286384b7cc1e6e5d1e84f350ba399014e

---
+++ _nuxt/static/1669058027/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1669058027",layout:"default",error:a,serverRendered:true,routePath:b,config:{baseUrl:"https:\u002F\u002Fubdmf.github.io\u002F",_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"06fc5ca3"}}}}(null,"\u002F"));
