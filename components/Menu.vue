5d9b2effd5706336c7bc418385fabd22895ae496
kyoapps-kyo <kyo@kyoapps.com>
2022-11-22 12:08:53 +0900

    为菜单添加过渡动画

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-    <ul class="flex flex-col md:flex-row items-center gap-x-0 md:gap-x-4">
-      <li>
+    <ul
+      class="flex flex-col md:flex-row items-center gap-x-0 md:gap-x-4 text-gray-300"
+    >
+      <li class="hover:underline hover:text-sky-300 transition duration-500">
         <nuxt-link to="/categories">笔记本列表</nuxt-link>
       </li>
-      <li>
+      <li class="hover:underline hover:text-sky-300 transition duration-500">
         <nuxt-link to="/tags">标签归纳</nuxt-link>
       </li>
-      <li>
+      <li class="hover:underline hover:text-sky-300 transition duration-500">
         <nuxt-link to="/articles">笔记列表</nuxt-link>
       </li>
     </ul>
