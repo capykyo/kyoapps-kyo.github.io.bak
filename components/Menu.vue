1cc69342309e815670d063cd17fbc977730fb939
kyoapps-kyo <kyo@kyoapps.com>
2022-11-22 11:41:43 +0900

    menu add categories

--- components/Menu.vue
+++ components/Menu.vue
@@ -2,10 +2,13 @@
   <div>
     <ul class="flex flex-col md:flex-row items-center gap-x-0 md:gap-x-4">
       <li>
-        <nuxt-link to="/tags">tags</nuxt-link>
+        <nuxt-link to="/categories">笔记本列表</nuxt-link>
       </li>
       <li>
-        <nuxt-link to="/articles">articles</nuxt-link>
+        <nuxt-link to="/tags">标签归纳</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/articles">笔记列表</nuxt-link>
       </li>
     </ul>
   </div>
