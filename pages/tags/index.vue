4ff2cf2e59f82b74d618e0e5f14e49f292a2c226
kyoapps-kyo <kyo@kyoapps.com>
2022-11-22 14:14:43 +0900

    修改tag标签背景色

--- pages/tags/index.vue
+++ pages/tags/index.vue
@@ -2,7 +2,7 @@
   <div
     class="text-gray-700 dark:text-gray-200 max-w-3xl mx-auto bg-white/40 rounded-lg blur-md shadow py-10 px-14"
   >
-    <h2 class="text-left text-lg mb-8">标签列表页</h2>
+    <h2 class="text-left text-lg mb-8">标签列表</h2>
     <div
       v-for="[index, item] of alphabetList.entries()"
       :key="item"
@@ -19,8 +19,8 @@
         >
           <nuxt-link
             :to="{ name: 'tags-tag', params: { tag: tag.toLowerCase() } }"
-            class="text-xl hover:underline px-2 p-1 rounded"
-            :class="setColor(index)"
+            class="text-xl dark:text-gray-600 hover:underline px-2 p-1 rounded transition duration-300 hover:scale-105 hover:shadow"
+            :style="`background-color:${getColor(index)}`"
             >{{ tag }}</nuxt-link
           >
         </li>
@@ -46,19 +46,14 @@ export default {
   data() {
     return {
       color: [
-        'rose',
-        'red',
-        'orange',
-        'amber',
-        'emerald',
-        'teal',
-        'cyan',
-        'blue',
-        'purple',
-        'fuchsia',
-        'slate',
-        'stone',
-        'zinc',
+        '#FFADAD',
+        '#FFD6A5',
+        '#FDFFB6',
+        '#CAFFBF',
+        '#9BF6FF',
+        '#A0C4FF',
+        '#BDB2FF',
+        '#FFC6FF',
       ],
     }
   },
@@ -105,10 +100,15 @@ export default {
     },
   },
   methods: {
-    setColor(index) {
-      const name = this.color[index]
-      const number = (parseInt(Math.random() * 4) + 3) * 100
-      return `bg-${name}-${number}`
+    currIndex(index) {
+      const color = this.color
+      if (index >= color.length) {
+        return index % color.length
+      } else return index
+    },
+    getColor(index) {
+      const currIndex = this.currIndex(index)
+      return this.color[currIndex]
     },
   },
 }
