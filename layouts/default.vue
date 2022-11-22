4a0aa48cb63d6e11fe3ab5ff45d8c442a1a7b34a
kyoapps-kyo <kyo@kyoapps.com>
2022-11-22 11:12:00 +0900

    layouts defaut 背景

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col justify-between min-h-[100vh]">
     <Header />
     <main
-      class="bg-gradient-to-br from-emerald-200/70 to-fuchsia-200/70 dark:from-violet-900 dark:to-slate-900 pt-5 flex-grow"
+      class="bg-gradient-to-br from-emerald-200/70 to-fuchsia-200/70 dark:from-indigo-900 dark:to-slate-900 pt-5 flex-grow"
     >
       <div class="container mx-auto px-4 pt-[80px] pb-10">
         <Nuxt />
